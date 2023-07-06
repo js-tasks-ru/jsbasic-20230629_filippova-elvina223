1def0ed524eaca7f2f85e7a20bfc7073186ae7d0
Elvina Filippova <101575573+elvina223@users.noreply.github.com>
2023-07-06 16:34:39 +0300

    Update index.js

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,8 @@
-function truncate(str, maxlength) {
-  // ваш код...
-}
+function truncate(str, maxlength){
+  let newStr=(str.slice(0, maxlength - 1) + '…');
+  if(str.length>maxlength) return newStr;
+        return str
+  }
+
+ console.log(truncate('Вот, что мне хотелось бы сказать на эту тему:', 20));
+ console.log(truncate('Всем привет!', 20));
