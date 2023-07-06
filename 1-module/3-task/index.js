e32c3b6730e5ca965c89945a30f524c2f9228579
Elvina Filippova <101575573+elvina223@users.noreply.github.com>
2023-07-06 17:03:08 +0300

    Update index.js

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,8 @@
 function ucFirst(str) {
+  function ucFirst(str) {
+  if(str=='' && str.length>='1') return str;
+  
+   return str[0].toUpperCase() + str.slice(1);
   // ваш код...
 }
+  ucFirst('');
