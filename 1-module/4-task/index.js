0a5ef7e6139673bfd221e4edfc639f3a7720a8e6
Elvina Filippova <101575573+elvina223@users.noreply.github.com>
2023-07-06 14:06:49 +0300

    Update index.js

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,11 @@
-function checkSpam(str) {
-  // ваш код...
-}
+
+  function checkSpam(str){
+  let strNew = str.toLowerCase();
+     
+    if (strNew.includes('1xbet') || strNew.includes('xxx'))return true;
+   return false
+   }
+
+ console.log(checkSpam('1Xbet now'));
+ console.log(checkSpam('free xxxX'));
+ console.log(checkSpam('innocent rabbit'));
