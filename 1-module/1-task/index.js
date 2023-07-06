41db5e3ce257f9f43dcf07cd1cf03ef0be51a2c0
Elvina Filippova <101575573+elvina223@users.noreply.github.com>
2023-07-06 16:49:38 +0300

    Update index.js

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,15 @@
-function factorial(n) {
+
+  function factorial(n) {
+    let res=1;
+    for (let i = 1; i<=n; i++) {
+        res *=i;
+    }
+    console.log(res)
+    }
+  factorial(0); // 1
+  factorial(1); // 1
+  factorial(3); // 6
+  factorial(5); // 120
+ 
   // ваш код...
 }
